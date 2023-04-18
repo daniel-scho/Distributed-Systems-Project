f97f9a3a66e6669c14a929953a78454097a68013
daniel-scho <bs.daniel.scho@gmail.com>
2023-04-18 13:22:30 +0200

    Connecting SpringBootApp with JavaFx

--- JavaFX/src/main/java/module-info.java
+++ JavaFX/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.fh.javafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.net.http;
 
     opens com.fh.javafx to javafx.fxml;
     exports com.fh.javafx;
