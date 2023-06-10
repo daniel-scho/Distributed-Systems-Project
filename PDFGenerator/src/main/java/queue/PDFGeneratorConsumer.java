cd0be505ede16269e98a6ceceda1bf7985c326ac
LorinFH <wi21b097@technikum-wien.at>
2023-06-10 12:49:42 +0200

    PDF Generator + QUEUE

--- PDFGenerator/src/main/java/queue/PDFGeneratorConsumer.java
+++ PDFGenerator/src/main/java/queue/PDFGeneratorConsumer.java
@@ -1,4 +1,41 @@
 package queue;
 
+import com.rabbitmq.client.*;
+
+import service.PDFGeneratorService;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class PDFGeneratorConsumer {
+    private final static String QUEUE_TO_PDF_GENERATOR = "PDFGeneratorQueue";
+
+    public PDFGeneratorConsumer(PDFGeneratorService pdfGeneratorService) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(30003);
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_TO_PDF_GENERATOR, false, false, false, null);
+
+
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String json = new String(body, StandardCharsets.UTF_8);
+                System.out.println("Received JSON: " + json);
+
+                // Aufruf des PDFGeneratorService, um PDF zu generieren und zu speichern
+                pdfGeneratorService.generateAndSavePDF(json);
+
+                // Bestätigung der erfolgreichen Verarbeitung der Nachricht
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(QUEUE_TO_PDF_GENERATOR, false, consumer);
+        System.out.println("PDFGeneratorConsumer is listening for messages...");
+    }
 }
---
+++ PDFGenerator/src/main/java/service/PDFGeneratorService.java
@@ -0,0 +1,44 @@
+package service;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PDFGeneratorService {
+
+    public void generateAndSavePDF(String customerData) {
+        // Hier kannst du die Logik zum Generieren der PDF basierend auf den JSON-Daten implementieren
+        // In diesem Beispiel wird das JSON einfach in den PDF-Inhalt geschrieben und im Dateisystem gespeichert
+
+        try {
+            // Erstellen eines neuen PDF-Dokuments
+            Document document = new Document();
+
+            // Definieren des Dateinamens für die gespeicherte PDF
+            String fileName = "Invoice.pdf";
+
+            // Erstellen einer PdfWriter-Instanz, um das Dokument in eine PDF-Datei zu schreiben
+            PdfWriter.getInstance(document, new FileOutputStream(fileName));
+
+            // Öffnen des Dokuments zum Schreiben
+            document.open();
+
+            // Schreiben des JSON-Inhalts als Paragraph in das PDF-Dokument
+            document.add(new Paragraph(customerData));
+
+            // Schließen des Dokuments
+            document.close();
+
+            System.out.println("PDF generated and saved successfully.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
